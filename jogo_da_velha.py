f5c8a0e0d9151fb2a5781c4d54e2e8b813dce833
Matheus Bione <mathenbiolis@gmail.com>
2022-05-29 11:29:22 +0000

    Adicionados comentários, corrigidos bugs

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,9 +1,10 @@
-# Biblioteca necessária para uso da função sleep()
+"""Biblioteca necessária para uso da função sleep()"""
 import time
 
 
-# Método com um array como argumento para mostrar o tabuleiro
-def tabuleiro(elemento: list) -> None:
+def tabuleiro(elemento: list[str]) -> None:
+    """Método com uma lista de strings como argumento
+    para mostrar o tabuleiro"""
     print()
     print(f"{elemento[0]} | {elemento[1]} | {elemento[2]}")
     print(f"{elemento[3]} | {elemento[4]} | {elemento[5]}")
@@ -11,9 +12,11 @@ def tabuleiro(elemento: list) -> None:
 
 
 print("Bem vindo à Jogo da Velha! pressione ENTER para começar:")
-# O input() por si espera um uma confirmação de um dispositivo de entrada
 
-print("\nCarregando...")
+# O input() por si espera um uma confirmação de um dispositivo de entrada
+input()
+
+print("Carregando...")
 # O sistema fica ocioso por 3 segundos
 time.sleep(3)
 # O print() por si apenas imprime uma nova linha
@@ -28,6 +31,7 @@ print("\nComo jogar:")
 print("Cada casa do jogo da velha tem um número associado.")
 print("Veja a ilustração:")
 
+# Usa-se a função tabuleiro() para mostrar o tabuleiro de exemplo
 tabuleiro([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 print("\nPara ganhar, você precisa conseguir 3 em linha,")
@@ -35,10 +39,12 @@ print("na horizontal, na vertical ou na diagonal.\n")
 
 time.sleep(5)
 
+# Este é um tipo de formatação chamado de f-string
 print(f"{jogador1}(bolinha) vs {jogador2}(x)!")
 
 
-def checar_vitoria(elemento: list) -> bool:
+def checar_vitoria(elemento: list[str]) -> bool:
+    """Método que checa se há vitoria a partir de uma lista de strings"""
     for i in range(3):
 
         # Se a casa 1, 2 e 3 estão com o mesmo símbolo, alguém ganhou
@@ -60,7 +66,18 @@ def checar_vitoria(elemento: list) -> bool:
     return False
 
 
-def rodada(elemento: list, num_jogador, num_rodada, jogador: str) -> None:
+def checar_empate(elemento: list[str]) -> bool:
+    """Método que checa se há empate a partir de uma lista de strings"""
+    for i in range(9):
+        if elemento[i] == " ":
+            return False
+
+    return True
+
+
+def rodada(elemento: list[str], num_jogador, num_rodada, jogador: str) -> None:
+    """Método "principal" que recebe inúmeros argumentos e
+    implementa as outras funções"""
     if num_jogador == 1:
         simbolo = "O"
     else:
@@ -69,7 +86,7 @@ def rodada(elemento: list, num_jogador, num_rodada, jogador: str) -> None:
     # Note que é possível utilizar métodos dentro de métodos
     tabuleiro(elemento)
 
-    print("Rodada {}!".format(num_rodada))
+    print(f"Rodada {num_rodada}!")
     print(f"{jogador}, digite o número da casa que deseja jogar:")
 
     jogada = int(input())
@@ -90,30 +107,33 @@ def rodada(elemento: list, num_jogador, num_rodada, jogador: str) -> None:
         rodada(elemento, num_jogador, num_rodada, jogador)
 
 
-elemento = [" "] * 9
-num_rodada = 1
+elemento_do_jogo = [" "] * 9
 # Se o contador for par, é a vez do jogador 1, senão, é a vez do jogador 2
-contador_jogador = 0
 
-while True:
-    if contador_jogador % 2 == 0:
-        jogador = jogador1
-        num_jogador = 1
-    else:
-        jogador = jogador2
-        num_jogador = 2
+for num_rodada_atual in range(1, 10):
+    contador_jogador = num_rodada_atual - 1
 
-    rodada(elemento, num_jogador, num_rodada, jogador)
+    # O operador not serve para inverter o valor entre 0 e 1,
+    # ou seja, se o contador for par, o número do jogador é 1, e vice-versa
+    num_jogador_atual = not contador_jogador % 2
 
-    if checar_vitoria(elemento) is True:
+    # Operador ternário do python. Com isso, é possível fazer um if statement
+    # com apenas uma linha, sem repetir o "jogador_atual = "
+    jogador_atual = jogador1 if num_jogador_atual == 1 else jogador2
 
-        tabuleiro(elemento)
+    rodada(elemento_do_jogo, num_jogador_atual,
+           num_rodada_atual, jogador_atual)
 
-        print(f"{jogador} venceu!, Parabéns")
-        print(f"Você venceu na {num_rodada}ª rodada!")
+    if checar_vitoria(elemento_do_jogo) is True:
+
+        tabuleiro(elemento_do_jogo)
+
+        print(f"{jogador_atual} venceu!, Parabéns")
+        print(f"Você venceu na {num_rodada_atual}ª rodada!")
         break
 
-    num_rodada += 1
-    contador_jogador += 1
+    if checar_empate(elemento_do_jogo) is True:
+        print("Empate!")
+        break
 
     time.sleep(0.5)
